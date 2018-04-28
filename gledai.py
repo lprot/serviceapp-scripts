f0ccb567e91d7aa3d8823d392f1e8fa9bb8db824
lprot <lprot@list.ru>
2018-04-28 22:07:55 +0300

    new host:   gledai.py

--- gledai.py
+++ gledai.py
@@ -1,13 +1,14 @@
 import urllib2, re, sys
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-r = 'http://bg-gledai.org/geto2.php?my=' + sys.argv[1]
+r = 'http://www.bg-gledai.tv/new/geto2.php?my=' + sys.argv[1]
+h = 'www.bg-gledai.tv'
 q = urllib2.Request(r)
-q.add_header('Host', 'bg-gledai.org')
+q.add_header('Host', h)
 q.add_header('Referer', 'http://' + sys.argv[2])
 q.add_header('User-Agent', ua)
 s = urllib2.unquote(re.search(r"unescape\(\'(.*?)\'", urllib2.unquote(urllib2.urlopen(q).read())).group(1))
 q = urllib2.Request(s)
-q.add_header('Host', 'bg-gledai.org')
+q.add_header('Host', h)
 q.add_header('Referer', r)
 q.add_header('User-Agent', ua)
 s = re.search(r"file>(.*?)<", urllib2.urlopen(q).read()).group(1).replace('&amp;','&')
