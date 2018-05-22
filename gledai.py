4762fb2365d2d069f1a685b016826528d869e439
lprot <lprot@list.ru>
2018-05-23 00:19:37 +0300

    host change fix:   gledai.py

--- gledai.py
+++ gledai.py
@@ -1,7 +1,7 @@
 import urllib2, re, sys
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-r = 'http://www.bg-gledai.tv/new/geto2.php?my=' + sys.argv[1]
-h = 'www.bg-gledai.tv'
+h = 'www.bg-gledai.me'
+r = 'http://' + h + '/new/geto2.php?my=' + sys.argv[1]
 q = urllib2.Request(r)
 q.add_header('Host', h)
 q.add_header('Referer', 'http://' + sys.argv[2])
@@ -12,4 +12,4 @@ q.add_header('Host', h)
 q.add_header('Referer', r)
 q.add_header('User-Agent', ua)
 s = re.search(r"file>(.*?)<", urllib2.urlopen(q).read()).group(1).replace('&amp;','&')
-print str(s) + '#Origin=http%3a//www.bg-gledai.tv&Referer=' + r.replace(':','%3a') + '&User-Agent=' + ua
+print str(s) + '#Origin=http%3a//' + h + '&Referer=' + r.replace(':','%3a') + '&User-Agent=' + ua
